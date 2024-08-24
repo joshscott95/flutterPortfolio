b66f26d1bb6b45b3208702b12d629f1858b83187
joshscott95 <joshscott95@yahoo.com>
2024-08-23 23:58:09 -0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'JCS',
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
