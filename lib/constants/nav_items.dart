14fb715a0002428324e0dd82283171533b6c7225
joshscott95 <joshscott95@yahoo.com>
2024-08-25 15:28:15 -0700

    added two more widgets drawer_mobile and main_desktop as well as added some assets

--- lib/constants/nav_items.dart
+++ lib/constants/nav_items.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 List<String> navTitles = [
   "Home",
   "Skills",
   "Projects",
   "Blog",
   "Contact",
+];
+
+List<IconData> navIcons = [
+  Icons.home,
+  Icons.android_outlined,
+  Icons.back_hand,
+  Icons.web,
+  Icons.quick_contacts_mail,
 ];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutterportfolio/constants/colors.dart';
+import 'package:flutterportfolio/constants/drawer_constraints.dart';
 import 'package:flutterportfolio/constants/nav_items.dart';
 import 'package:flutterportfolio/styles/style.dart';
+import 'package:flutterportfolio/widgets/drawer_mobile.dart';
 import 'package:flutterportfolio/widgets/header_desktop.dart';
 import 'package:flutterportfolio/widgets/header_mobile.dart';
+import 'package:flutterportfolio/widgets/main_desktop.dart';
 import 'package:flutterportfolio/widgets/site_logo.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,27 +17,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: CustomColor.scaffoldBG,
-        body: ListView(
-          scrollDirection: Axis.vertical,
-          children: [
+    final screenSize = MediaQuery.of(context).size;
+    final screenWidth = screenSize.width;
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: CustomColor.scaffoldBG,
+          endDrawer: constraints.maxWidth >= kMinDekstopWidth
+              ? null
+              : const DrawerMobile(),
+          body: ListView(scrollDirection: Axis.vertical, children: [
             // MAIN
-            // HeaderDesktop(),
-            HeaderMobile(
-              onLogoTap: () {},
-              onMenuTap: () {},
-            ),
-            
-            
+            if (constraints.maxWidth >= kMinDekstopWidth)
+              const HeaderDesktop()
+            else
+              HeaderMobile(
+                onLogoTap: () {},
+                onMenuTap: () {
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+              ),
+
+            const MainDesktop(),
+
             //SKILLS
             Container(
                 height: 500,
                 width: double.maxFinite,
-                color: Colors.blueGrey
-                ), //PROJECTS
+                color: Color.fromRGBO(26, 31, 54, 1)), //PROJECTS
             Container(
               height: 500,
               width: double.maxFinite,
@@ -42,13 +56,12 @@ class _HomePageState extends State<HomePage> {
             Container(
                 height: 500,
                 width: double.maxFinite,
-                color: Colors.blueGrey), //FOOTER
+                color: Color.fromRGBO(26, 31, 54, 1)), //FOOTER
             Container(
               height: 500,
               width: double.maxFinite,
             ),
-        ]
-    ));
-        
+          ]));
+    });
   }
 }
